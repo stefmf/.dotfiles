85df757f21f6fe3b80f8b6ddd3e29846467a776e
Stephen Monclova <astro@Dreams.local>
2024-11-08 11:49:45 -0500

    various changes

---
+++ .config/dock/dock_config.zsh
@@ -0,0 +1,32 @@
+#!/bin/zsh
+
+# Remove all current dock items
+dockutil --remove all --no-restart
+
+# Add items in specified order
+dockutil --add /System/Applications/Launchpad.app --no-restart
+dockutil --add /Applications/iTerm.app --no-restart
+dockutil --add /Applications/Sublime\ Text.app --no-restart
+dockutil --add /Applications/Visual\ Studio\ Code.app --no-restart
+dockutil --add /Applications/Claude.app --no-restart
+dockutil --add /Applications/Windows\ App.app --no-restart
+dockutil --add /Applications/Parallels\ Desktop.app --no-restart
+dockutil --add /System/Applications/Utilities/Screen\ Sharing.app --no-restart
+dockutil --add /System/Applications/Calendar.app --no-restart
+dockutil --add /System/Applications/Notes.app --no-restart
+dockutil --add /Applications/Firefox.app --no-restart
+dockutil --add /Applications/Spotify.app --no-restart
+dockutil --add /Applications/Discord.app --no-restart
+dockutil --add /System/Applications/Messages.app --no-restart
+dockutil --add /System/Applications/iPhone\ Mirroring.app --no-restart
+dockutil --add /System/Applications/System\ Settings.app --no-restart
+
+# Add Downloads folder as a stack in list view
+dockutil --add '~/Downloads' --view list --display stack --no-restart
+
+# Set additional Dock preferences
+defaults write com.apple.dock minimize-to-application -bool true # Minimize into app icon
+defaults write com.apple.dock show-recents -bool false # Disable recent apps
+
+# Restart the Dock to apply changes
+killall Dock
